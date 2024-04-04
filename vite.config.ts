cdd51fb4526770dcbc13ce5cb4b5af9ff919ce53
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-04-03 22:48:38 -0400

    404 for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import path from "path";
+import path, { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ghPages404: resolve(__dirname, "404.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
